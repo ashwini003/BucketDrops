d79d34a9d0abcdfff4335944df87151e13af2217
Admin <ashwinics003@gmail.com>
2018-06-07 18:15:33 +0530

    main screen activity done

--- app/src/main/java/com/project/com/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/project/com/bucketdrops/ActivityMain.java
@@ -2,12 +2,29 @@ package com.project.com.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
+    Toolbar mtoolbar ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mtoolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+        initBackgroundImage();
+
+    }
+
+    private void initBackgroundImage() {
+
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .into(background);
     }
 }
