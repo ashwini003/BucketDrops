6b5fe26944b550152c18e7b47bd7e3a062a7ed4f
Admin <ashwinics003@gmail.com>
2018-06-08 14:25:51 +0530

    dialogue box

--- app/src/main/java/com/project/com/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/project/com/bucketdrops/ActivityMain.java
@@ -3,19 +3,26 @@ package com.project.com.bucketdrops;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements View.OnClickListener {
 
     Toolbar mtoolbar ;
+    Button mbtn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mtoolbar=(Toolbar) findViewById(R.id.toolbar);
+        mbtn = (Button) findViewById(R.id.btn_add);
+        mbtn.setOnClickListener(this);
         setSupportActionBar(mtoolbar);
+
         initBackgroundImage();
 
     }
@@ -26,5 +33,18 @@ public class ActivityMain extends AppCompatActivity {
         Glide.with(this)
                 .load(R.drawable.background)
                 .into(background);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        showDialogAdd();
+        
+    }
+
+    private void showDialogAdd() {
+            DialogAdd dialog = new DialogAdd();
+            dialog.show(getSupportFragmentManager(),"ADD");
+
     }
 }
